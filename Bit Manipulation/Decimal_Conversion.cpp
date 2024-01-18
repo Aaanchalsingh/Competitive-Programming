7229b1775dfbaaa7049f6752c5795e9a2b11b209
Aanchal Singh <aanchalsinghuk@gmail.com>
2024-01-18 10:07:16 +0530

    18_Jan_23

---
+++ Bit Manipulation/Decimal_Conversion.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+// To convert Decimal To Binary take LCM and Store Remainder in String
+// s+=p takes more time than s.append(p) which does it in o(1) time
+// To find for negative numbers like -10 first find the binary of 10 and take its 2s comple
+int main()
+{
+    int x = 10;
+    string binary = "";
+    while (x != 0)
+    {
+        int rem = x % 2;
+        binary += to_string(rem);
+        x /= 2;
+    }
+    reverse(binary.begin(), binary.end());
+    cout << binary << endl;
+    cout << ~5 << endl; // This will give -(x+1)
+    // This doesnot simply imply to unsigned int
+    cout << ~(~5) << endl;
+    return 0;
+}
