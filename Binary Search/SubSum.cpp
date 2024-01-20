d3a07babe4210465fb3ee96b966405f7288362a7
Aanchal Singh <aanchalsinghuk@gmail.com>
2024-01-20 10:04:02 +0530

    latest

---
+++ Binary Search/SubSum.cpp
@@ -0,0 +1,8 @@
+//SUBSUMS - Subset Sums On SPOJ
+/*Given a sequence of N (1 ≤ N ≤ 34) numbers S1, ..., SN (-20,000,000 ≤ Si ≤ 20,000,000), determine how many subsets of S (including the empty one) have a sum between A and B (-500,000,000 ≤ A ≤ B ≤ 500,000,000), inclusive.*/
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+
+return 0;
+}
