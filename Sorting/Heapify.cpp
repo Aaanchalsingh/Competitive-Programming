8cd4dd3fd4df6ade2517ca0e436c1fe6659ae570
Aanchal Singh <aanchalsinghuk@gmail.com>
2024-01-23 14:53:37 +0530

    L

--- Sorting/Heapify.cpp
+++ Sorting/Heapify.cpp
@@ -1,79 +1,87 @@
-// Heap Creation
 #include <bits/stdc++.h>
 using namespace std;
+
 class Heap
 {
 public:
     vector<int> v;
+    int size;
+
+    Heap()
+    {
+        size = 0;
+    }
+
     void push(int data)
     {
         int n = v.size(), i = n;
         v.push_back(data);
-        while ((i >= 1) and (v[i] > v[i / 2]))
+        while (i > 0 && v[i] > v[i / 2])
         {
             swap(v[i], v[i / 2]);
             i = i / 2;
         }
+        size++;
     }
-    void heapify()
-    {
-        int n = v.size() - 1, i = 0;
-        while (i <= n)
-        {
-            int largest = i;
-            if (i * 2 <= n && v[i] < v[i * 2])
-                largest = i * 2;
 
-            if (i * 2 + 1 <= n && v[largest] < v[i * 2 + 1])
-                largest = i * 2 + 1;
-            if (largest != i)
-            {
-                swap(v[i], v[largest]);
-                i = largest;
-            }
-            else
-                break;
+    void heapify(int i)
+    {
+        int n = size;
+        int largest = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+
+        if (left < n && v[left] > v[largest])
+            largest = left;
+
+        if (right < n && v[right] > v[largest])
+            largest = right;
+
+        if (largest != i)
+        {
+            swap(v[i], v[largest]);
+            heapify(largest);
         }
     }
+
     void pop()
     {
-        int n = v.size();
+        int n = size;
         if (n == 0)
             return;
         else
         {
-            int k = v[0];
-            swap(v[n - 1], v[0]);
-            v.pop_back();
-            for (auto x : v)
-                cout << x << " ";
-            cout << endl;
-            heapify();
-            v.push_back(k);
-            for (auto x : v)
-                cout << x << " ";
-            cout << endl;
+            size--;
+            swap(v[size], v[0]);
+            heapify(0);
         }
     }
+
     int top()
     {
         return v[0];
     }
-    /*
-    Creating/Building a heap inplace O(N) Time Complexity
-    heapsort O(NLogN) but inplace remove it like before in this code
-     for(int i=n/2;i>=0;i--){
-        heapify(i);
-    }
-    */
 };
+
 int main()
 {
     Heap obj;
     for (int i = 0; i < 10; i++)
         obj.push(i + 1);
-    for (int i = 0; i < 10; i++)
-        obj.pop();
+    obj.push(-1);
+    obj.push(100);
+    obj.push(5);
 
+    cout << "Original Max Heap: ";
+    for (auto x : obj.v)
+        cout << x << " ";
+    cout << endl;
+
+    cout << "Sorted Order: ";
+    while (obj.size > 0)
+        obj.pop();
+    for (auto x : obj.v)
+        cout << x << " ";
+    cout << endl;
     return 0;
 }
