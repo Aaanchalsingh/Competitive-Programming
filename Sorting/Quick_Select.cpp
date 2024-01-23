e9d2dd7f618744716c2a69a1ec200b2142dfa912
Aanchal Singh <aanchalsinghuk@gmail.com>
2024-01-23 15:27:32 +0530

    latest

--- Sorting/Quick_Select.cpp
+++ Sorting/Quick_Select.cpp
@@ -1,42 +1,34 @@
 // QUICK SELECT O(N) Find Kth Smallest Element
 #include <bits/stdc++.h>
 using namespace std;
-int Partition(vector<int> &A, int low, int high)
+int Partition(vector<int> &v, int low, int high)
 {
-    int pivot = A[low];
-    int i = low + 1, j = high - 1;
-    while (i < j)
+    int j = low - 1, pivot = v[high];
+    for (int i = low; i < high; i++)
     {
-        while (A[i] <= pivot)
-            i++;
-        while (A[j] > pivot)
-            j--;
-        if (i < j)
-            swap(A[i], A[j]);
+        if (v[i] < pivot)
+            swap(v[i], v[++j]);
     }
-    swap(A[low], A[j]);
-    return j;
+    swap(v[j + 1], v[high]);
+    return j + 1;
 }
 int Quick_Select(vector<int> &A, int low, int high, int k)
 {
-    int mid;
-    if (low < high)
-    {
-        mid = Partition(A, low, high);
-        if (k == mid)
-            return A[mid];
-        if (mid > k)
-            Quick_Select(A, low, mid, k);
-        else
-            Quick_Select(A, mid + 1, high, k);
-    }
-    else
+    int p;
+    if (low >= high)
         return -1;
+    p = Partition(A, low, high);
+    if (k == p)
+        return A[p];
+    if (p > k)
+        Quick_Select(A, low, p - 1, k);
+    else
+        Quick_Select(A, p + 1, high, k);
 }
 int main()
 {
     vector<int> v{5, 4, 3, 2, 6, 7, 8, 1, 0, 9};
     for (int i = 0; i < v.size(); i++)
-        cout << Quick_Select(v, 0, v.size(), i + 1) << " ";
+        cout << Quick_Select(v, 0, v.size(), i) << " ";
     return 0;
 }
