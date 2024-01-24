280ed4a43174f7efdf13f5988e5bbd556f3dd004
Aanchal Singh <aanchalsinghuk@gmail.com>
2024-01-24 18:47:47 +0530

    latest

--- Greedy/Activity_Selection.cpp
+++ Greedy/Activity_Selection.cpp
@@ -3,22 +3,17 @@ using namespace std;
 
 int main()
 {
-    vector<int> start{1,3,2,5};
-    vector<int> end{2,4,3,6};
-    vector<int> ans;
-    int n = start.size();
-
+    vector<int> start{1, 3, 0, 5, 8, 5};
+    vector<int> end{2, 4, 6, 7, 9, 9};
+    int ans = 0, n = start.size();
     priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-
     for (int i = 0; i < n; i++)
-        pq.push(make_pair(end[i], start[i]));
-
+        pq.push({end[i], start[i]});
     auto it = pq.top();
     int currStart = it.second;
     int currEnd = it.first;
     pq.pop();
-    ans.push_back(currStart);
-
+    ans++;
     while (!pq.empty())
     {
         auto it = pq.top();
@@ -26,15 +21,29 @@ int main()
         int newEnd = it.first;
         pq.pop();
 
-        if (newStart >= currEnd)
+        if (newStart > currEnd)
         {
-            ans.push_back(newStart);
+            ans++;
             currEnd = newEnd;
         }
     }
-
-    for (auto x : ans)
-        cout << x << " ";
+    cout << ans;
+    // Another Approach
+    vector<pair<int, int>> v;
+    for (int i = 0; i < n; i++)
+        v.push_back(make_pair(end[i], start[i]));
+    sort(v.begin(), v.end());
+    int x = 0;
+    int ans = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (v[i].second > x)
+        {
+            ans++;
+            x = v[i].first;
+        }
+    }
+    cout<< ans;
 
     return 0;
 }
