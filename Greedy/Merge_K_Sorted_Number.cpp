b2682b6fb4c6ea2ee29752057aa9d242fed68865
Aanchal Singh <aanchalsinghuk@gmail.com>
2024-01-25 19:40:07 +0530

    l

--- Greedy/Merge_K_Sorted_Number.cpp
+++ Greedy/Merge_K_Sorted_Number.cpp
@@ -1,19 +1,16 @@
-// MERGE K SORTED ARRAY
 #include <bits/stdc++.h>
 using namespace std;
+
 int main()
 {
     int k = 3, n = 4;
     vector<vector<int>> v{{1, 3, 5, 7}, {2, 4, 6, 8}, {0, 9, 10, 11}};
-    priority_queue<vector<int>, vector<vector<int>>, greater<int>> min_heap;
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> minHeap;
     for (int i = 0; i < k; i++)
-        min_heap.push({v[i][0], i});
-    for (int i = 0; i < n; i++)
+        minHeap.push({v[i][0], i});
+    while (!minHeap.empty())
     {
-        if (!min_heap.empty())
-            cout << min_heap.top().first << " ";
-        min_heap.pop();
-        min_heap.push({v[i][0], i});
+        pair<int, int> p = minHeap.top();
     }
 
     return 0;
