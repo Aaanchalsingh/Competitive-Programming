44fd0524d0377e9b41dd3b981273772a28c7b045
Aanchal Singh <aanchalsinghuk@gmail.com>
2024-01-26 15:49:03 +0530

    latest

--- Basic/sqrt.cpp
+++ Basic/sqrt.cpp
@@ -1,50 +1,67 @@
-//SQRT OF A NUMBER
+// SQRT OF A NUMBER
 #include <bits/stdc++.h>
 using namespace std;
-int order_n(int n){
-    int i=0;
-    for(i=0;i*i<=n;i++){
-        if(i*i==n) return i;
+int order_n(int n)
+{
+    int i = 0;
+    for (i = 0; i * i <= n; i++)
+    {
+        if (i * i == n)
+            return i;
     }
-    return i-1;
+    return i - 1;
 }
-int order_logn(int n){
-    int low=0,end=n;
-    while(low<=end){
-        int mid = low-low/2+end/2;
-        if(mid*mid==n) return mid;
-        else if(mid*mid<n) low = mid + 1;
-        else end=mid-1;
+int order_logn(int n)
+{
+    int low = 0, end = n;
+    while (low <= end)
+    {
+        int mid = low - low / 2 + end / 2;
+        if (mid * mid == n)
+            return mid;
+        else if (mid * mid < n)
+            low = mid + 1;
+        else
+            end = mid - 1;
     }
     return end;
 }
-float order_n_precision(int n,int p){
-    float low=0,end=n,ans=-1;
-    while(low<=end){
-        int mid = low-low/2+end/2;
-        if(mid*mid==n){
-            ans=mid;
+float order_n_precision(int n, int p)
+{
+    float low = 0, end = n, ans = -1;
+    while (low <= end)
+    {
+        int mid = low - low / 2 + end / 2;
+        if (mid * mid == n)
+        {
+            ans = mid;
             break;
         }
-        else if(mid*mid<n) low = mid + 1;
-        else end=mid-1;
+        else if (mid * mid < n)
+            low = mid + 1;
+        else
+            end = mid - 1;
     }
-    if(ans==-1) ans=end;
+    if (ans == -1)
+        ans = end;
     float increment = 0.1;
-    for(int i=0;i<p;i++) {
-        while(ans*ans<=n){
-            ans+=increment;
+    for (int i = 0; i < p; i++)
+    {
+        while (ans * ans <= n)
+        {
+            ans += increment;
         }
-        ans-=increment;
-        increment=increment/10;//3.1//3.16//3.162
-        }
+        ans -= increment;
+        increment = increment / 10; // 3.1//3.16//3.162
+    }
     return ans;
 }
-int main(){
+int main()
+{
 
-    int sqrt=10;
-    cout<<"The square root of "<<sqrt<<" is : "<<order_n(sqrt)<<" O(N)"<<endl;
-    cout<<"The square root of "<<sqrt<<" is : "<<order_logn(sqrt)<<" O(LogN)"<<endl;
-    cout<<"The square root of "<<sqrt<<" is : "<<order_n_precision(sqrt,3)<<" With Precision"<<endl;
+    int sqrt = 10;
+    cout << "The square root of " << sqrt << " is : " << order_n(sqrt) << " O(N)" << endl;
+    cout << "The square root of " << sqrt << " is : " << order_logn(sqrt) << " O(LogN)" << endl;
+    cout << "The square root of " << sqrt << " is : " << order_n_precision(sqrt, 3) << " With Precision" << endl;
     return 0;
 }
--- Greedy/Expedition.cpp
+++ Greedy/Expedition.cpp
@@ -1,51 +1,53 @@
-// EXPEDITION SPOJ
 #include <bits/stdc++.h>
 using namespace std;
+
 int solve(vector<vector<int>> v, int destination)
 {
-    priority_queue<pair<int, int>> pq;
+    priority_queue<int> pq;
     int n = v.size(), tank = 10, ans = 0;
-    for (int i = 0; i < n - 1; i++)
+
+    for (int i = 0; i < n;)
     {
-        int curdistance = v[i + 1][0] - v[i][0];
+        int curdistance = (i == 0) ? v[i][0] : v[i][0] - v[i - 1][0];
         if (curdistance <= tank)
         {
             tank -= curdistance;
-            pq.push({v[i][0], v[i][1]});
+            pq.push(v[i][1]);
+            i++;
         }
         else
         {
-            while (curdistance > tank && !pq.empty())
-            {
-                pair<int, int> nextstop = pq.top();
-                pq.pop();
-                ans++;
-                tank += nextstop.second;
-                tank -= curdistance;
-            }
+            if (pq.empty())
+                return -1;
+
+            tank += pq.top();
+            pq.pop();
+            ans++;
         }
     }
+
     int curdistance = destination - v[n - 1][0];
     if (curdistance <= tank)
         return ans;
 
     while (curdistance > tank && !pq.empty())
     {
-        pair<int, int> nextstop = pq.top();
+        tank += pq.top();
         pq.pop();
         ans++;
-        tank += nextstop.second;
-        tank -= curdistance;
     }
+
     if (curdistance > tank)
         return -1;
+
     return ans;
 }
+
 int main()
 {
     vector<vector<int>> v{
         {10, 60}, {20, 30}, {30, 30}, {60, 40}};
     int n = v.size();
-    cout << solve(v,100);
+    cout << solve(v, 100);
     return 0;
 }
