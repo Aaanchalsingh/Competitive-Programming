1367bd2a752515e4604b46699c2b0bd33158acf6
Aanchal Singh <aanchalsinghuk@gmail.com>
2024-02-04 13:00:49 +0530

    l

--- demo.cpp
+++ demo.cpp
@@ -1,26 +1,47 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int longest_Seq(vector<int> v, int n)
+int solve(string str, int limit)
 {
-    vector<int> dp(n + 1, 1);
-    int ans = 0;
-    for (int i = 1; i < n; i++)
+    vector<int> ans;
+    int n = str.size();
+    if (n == 0)
+        return 0;
+    map<char, int> mp;
+    for (int i = 0; i < n; i++)
+        mp[str[i]]++;
+    for (auto x : mp)
     {
-        for (int j = 0; j < i; j++)
+        ans.push_back(x.second);
+    }
+    sort(ans.begin(), ans.end(), greater<int>());
+    // 3 2 1
+    n = ans.size();
+    for (int i = 0; i < n; i++)
+    {
+        if (ans[i] > limit)
         {
-            if (v[i] >= v[j])
-            {
-                dp[i] = max(dp[i], 1 + dp[j]);
-                ans = max(ans, dp[i]);
-            }
+            ans[i] -= limit;
+            break;
+        }
+        else
+        {
+            int key = limit;
+            limit -= ans[i];
+            ans[i] -= key;
         }
     }
-    return ans;
+    int cost = 0;
+    for (int i = 0; i < n; i++)
+    {
+        cost += (ans[i] * ans[i]);
+    }
+    return cost;
 }
 int main()
 {
-    vector<int> v{3, 2, 4, 1, 7, 8, 6, 10, 9};
-    int n = v.size();
-    cout << longest_Seq(v, n);
+    string str = "abcbac";
+    int limit = 2;
+    cout << solve(str, limit);
+    return 0;
 }
