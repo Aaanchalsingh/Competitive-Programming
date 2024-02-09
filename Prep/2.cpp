eb0a480273e0de50a7c6fa5df8eaf421827b92c7
Aanchal Singh <aanchalsinghuk@gmail.com>
2024-02-09 19:45:01 +0530

    latest

---
+++ Prep/2.cpp
@@ -0,0 +1,8 @@
+/*
+given a array of car Weights like [5,10,-5,-10] , sign of car weights show the direction of car.
+if two cars are in opposite directions, the car with less weight gets destroyed. like this [10,-5] => [10]
+if both weight is equal and opposite then both will get destroyed. like this [10,-10] => []
+you have return the final set of cars after collision.
+input: [5,10,-5,-10]
+output: [5]
+*/
