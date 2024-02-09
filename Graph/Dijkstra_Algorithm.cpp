66fdef355fbf3704757b9694b6f9cb305428c8b1
Aanchal Singh <aanchalsinghuk@gmail.com>
2024-02-09 20:50:33 +0530

    l

--- Graph/Dijkstra_Algorithm.cpp
+++ Graph/Dijkstra_Algorithm.cpp
@@ -7,8 +7,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void path(){
-
+void path(vector<vector<int>> v)
+{
+    int n = v.size();
+    vector<int> dist(n + 1, 1e9);
+    dist[0] = 0;
+    queue<pair<int, int>> q;
+    q.push({0, 0});
+    vector<bool> vis(n, false);
+    while (!q.empty())
+    {
+        int sr = q.front().first;
+        int wt = q.front().second;
+        q.pop();
+        vis[sr] = true;
+        for (auto x : v[sr])
+        {
+            if (!vis[x])
+            {
+                if (wt + v[sr][x] < dist[x])
+                {
+                    dist[x] = wt + v[sr][x];
+                    q.push({x, dist[x]});
+                }
+            }
+        }
+    }
+    for (int i = 1; i <= dist.size(); i++)
+        cout << dist[i] << " ";
 }
 int main()
 {
